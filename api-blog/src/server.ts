219cdc000075169d85354f5a65099c2d0d4c0135
rafabcanedo <rafaitecanedo@gmail.com>
2024-04-29 08:02:56 -0300

    update user ready: have a good week XD

--- api-blog/src/server.ts
+++ api-blog/src/server.ts
@@ -5,6 +5,8 @@ import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoClient } from "./database/mongo";
 import { MongoCreateUserRepository } from "./repository/create-user/mongo-create-user";
 import { CreateUserController } from "./controllers/create-user/create-user";
+import { MongoUpdateUserRepository } from "./repository/update-user/mongo-update-user";
+import { UpdateUserController } from "./controllers/update-user/update-user";
 
 const main = async () => {
  config();
@@ -34,6 +36,19 @@ const main = async () => {
   const { body, statusCode } = await createUserController.handle({
     body: req.body,
   });
+
+  res.status(statusCode).send(body);
+ })
+
+ app.patch("/users/:id", async (req, res) => {
+  const mongoUpdateUserRepository = new MongoUpdateUserRepository()
+
+  const updateUserController = new UpdateUserController(mongoUpdateUserRepository)
+
+  const { body, statusCode } = await updateUserController.handle({
+    body: req.body,
+    params: req.params
+  })
 
   res.status(statusCode).send(body);
  })
