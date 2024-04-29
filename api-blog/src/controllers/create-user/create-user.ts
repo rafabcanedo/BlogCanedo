fb087d2e4d36550ebabb57945a3071d426be6972
rafabcanedo <rafaitecanedo@gmail.com>
2024-04-29 19:32:45 -0300

    created helpers for http messages

--- api-blog/src/controllers/create-user/create-user.ts
+++ api-blog/src/controllers/create-user/create-user.ts
@@ -3,44 +3,34 @@ import validator from "validator";
 import { User } from "../../models/user";
 import { HttpRequest, HttpResponse, IController } from "../protocols";
 import { CreateUserParams, ICreateUserRepository } from "./protocols";
+import { badRequest, created, serverError } from "../helpers";
 
 export class CreateUserController implements IController {
  constructor(private readonly createUserRepository: ICreateUserRepository) {}
 
- async handle(httpRequest: HttpRequest<CreateUserParams>): Promise<HttpResponse<User>> {
+ async handle(httpRequest: HttpRequest<CreateUserParams>): Promise<HttpResponse<User | string>> {
     try {
       // Validation fields"
       const requiredFields = ["name", "email", "password", "phone"];
 
       for (const field of requiredFields) {
         if (!httpRequest?.body?.[field as keyof CreateUserParams]?.length) {
-          return {
-            statusCode: 400,
-            body: `Field ${field} is required`,
-          }
+          return badRequest(`Field ${field} is required`)
         }
       }
 
       const emailIsValid = validator.isEmail(httpRequest.body!.email);
 
       if (!emailIsValid) {
-       return {
-         statusCode: 400,
-         body: "Email is valid",
-       }
+       return badRequest("Email is invalid")
       }
 
      const user = await this.createUserRepository.createUser(httpRequest.body!);
 
-     return {
-        statusCode: 201,
-        body: user,
-     }
+     return created<User>(user)
+
     } catch (error) {
-      return {
-       statusCode: 500,
-       body: "Something went wrong",
-      }
+      return serverError()
     }
  }
 }
--- api-blog/src/controllers/protocols.ts
+++ api-blog/src/controllers/protocols.ts
@@ -1,7 +1,7 @@
 // types for StatusCode with T(generic type)
 export interface HttpResponse<T> {
  statusCode: number;
- body: T | string;
+ body: T;
 }
 
 export interface HttpRequest<B> {
